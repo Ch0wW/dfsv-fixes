bcc042498dcdbc3841e6041c6a68ed86d12f5ecd
Ch0WW <Ch0wW@baseq.fr>
2025-09-17 18:27:08 +0200

    Small fix in start-servers.

--- start-servers.sh
+++ start-servers.sh
@@ -19,6 +19,8 @@ printf "\nServer Hostname: $SV_BASE_HOSTNAME\nAdmin: $ADMIN_NAME\nRcon Password:
 
 echo "Setting up native server environment..."
 
+currdir=$(pwd)
+
 curr_port=27960
 echo "Starting servers natively..."
 
@@ -26,21 +28,22 @@ for sv_type in mixed cpm vq3 fastcaps teamruns freestyle;do
 	i=0
 	sv_qty="${sv_type}_count"
 	sv_sfx="${sv_type}_sfx"
-	while [[ $i -ne "${!sv_qty}" ]]
+	while [[ $i -lt "${!sv_qty}" ]]
 	do
+		# Always come back here
+		cd $currdir
+
 		curr_id="rs${curr_port}"
 		i=$(($i+1))
 		curr_name="${sv_type}_${i}"
 		curr_hostname="${SV_BASE_HOSTNAME} ${!sv_sfx} ${i}"
 
-		echo "Starting server: $curr_name on port $curr_port"
+		echo "Starting server: ${curr_name} on port ${curr_port}"
 
 		# Create server-specific directory
-		mkdir -p game/defrag/$curr_name
-		cp game/defrag/cfgs/${sv_type}.cfg game/defrag/$curr_name/main.cfg
+		mkdir -p ./game/defrag/$curr_name
 
 		# Start the server in background
-		cd game
 		export MDD_ENABLED=${MDD_ENABLED}
 		export RS_ID=${!curr_id}
 		export NAME_ID=${curr_name}
@@ -58,12 +61,8 @@ for sv_type in mixed cpm vq3 fastcaps teamruns freestyle;do
 		export SV_PRIVATE=${SV_PRIVATE}
 		export SV_PASSWORD=${SV_PASSWORD}
 		export SV_CONFIG=${sv_type}
+		(cd game && screen -mdS "${sv_type}_${i}" ./start.sh)
 
-		# Start server using existing start.sh script
-		screen -mdS "${sv_type}-${i}" ./start.sh
-#		SERVER_PID=$!
-
-		cd ../..
 		curr_port=$(($curr_port+1))
 	done
 done
