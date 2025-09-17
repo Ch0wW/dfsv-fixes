350fc2b8084b5ff4f735db7232762ca23f6efb37
Ch0WW <Ch0wW@baseq.fr>
2025-09-17 19:48:36 +0200

    Small cleanup of the starting script.

--- game/start.sh
+++ game/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Write the general settings provided in sv.conf
 printf "sets .admin-irc \"$ADMIN_IRC\"
 sets .admin-discord \"$ADMIN_DICORD\"
 sets .admin-mail \"$ADMIN_MAIL\"
@@ -12,8 +14,8 @@ seta df_sv_script_idleCfg \"${NAME_ID}/main.cfg\"
 seta g_log \"${NAME_ID}/${NAME_ID}.log\"
 " > defrag/$NAME_ID/main.cfg 2>&1
 
-if [ $MDD_ENABLED -eq 1 ]
-then
+# In case we enabled external modules, set them up
+if [ $MDD_ENABLED -eq 1 ]; then
   export VM_GAME="0"
   printf  "seta rs_enable \"1\"
 seta rs_mode \"0\"
@@ -26,16 +28,18 @@ seta rs_mod_path \"defrag/modules\"
 seta rs_banTimerReset \"0\"
 seta df_obs_KillObs \"1\"
 seta rs_modules \"admin challenge compare find login logout mapdownload mapinfo me my mytime oldmytime oldrank oldtime oldtop popular random rank rankings ranktime ratemap recent servers time top user version who\"
+seta rs_server_id \"${RS_ID}\"
 " >> defrag/$NAME_ID/main.cfg 2>&1
 else
   export VM_GAME="2"
 fi
 
-if [ $SV_PRIVATE -eq 1 ]
-then
-  printf "seta g_password \"$SV_PASSWORD\"\n" >> defrag/$NAME_ID/main.cfg 2>&1
+# Check if the server has to set a password
+if [ $SV_PRIVATE -eq 1 ]; then
+  printf "seta g_password \"${SV_PASSWORD}\"\n" >> defrag/$NAME_ID/main.cfg 2>&1
 fi
 
+# Change the default map according to the gamemode
 case $SV_TYPE in
 
   mixed | vq3 | cpm)
@@ -55,4 +59,30 @@ case $SV_TYPE in
     ;;
 esac
 
-./oDFe.ded +set fs_homepath . +set fs_basegame "baseq3/nfs" +set net_enabled 1 +set net_port $SV_PORT +set rs_server_id $RS_ID +set com_hunkmegs 256 +set sv_pure 0 +set sv_levelTimeReset 1 +set fs_game defrag +set dedicated 2 +set vm_game $VM_GAME +set ttycon_ansicolor 1 +set bot_enable 0 +exec cfgs/global.cfg +exec $NAME_ID/main.cfg +exec cfgs/${SV_CONFIG}.cfg
+
+SERVER_SETTINGS=(
+        
+        +set dedicated          "2"
+        +set ttycon_ansicolor   "1"
+        +set com_hunkmegs       "256"
+
+        # Basic Folders
+        +set fs_homepath        "."
+        +set fs_basegame        "baseq3/nfs"
+        +set fs_game            "defrag"
+
+        +set net_enabled        "1"
+        +set net_port           "${SV_PORT}"
+        +set sv_pure            "0"
+        +set vm_game            "${VM_GAME}"
+        +set sv_levelTimeReset  "1"
+        +set bot_enable         "0"
+
+        +exec                   "cfgs/global.cfg"
+        +exec                   "${NAME_ID}/main.cfg"
+        +exec                   "cfgs/${SV_CONFIG}.cfg"
+
+)
+
+# Start the server with our settings
+./oDFe.ded ${SERVER_SETTINGS[@]}
