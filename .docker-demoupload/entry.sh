79375b2a7597b35f98839e9cb496819d6f84b314
Ch0WW <Ch0wW@baseq.fr>
2025-09-21 03:22:34 +0200

    that mistake name....

--- .docker-demoupload/entry.sh
+++ .docker-demoupload/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Setup backup cron job with cron expression DEMO_FTPS_CRONTAB: ${DEMO_SFTP_CRONTAB}"
+echo "Setup backup cron job with cron expression DEMO_SFTP_CRONTAB: ${DEMO_SFTP_CRONTAB}"
 echo "${DEMO_SFTP_CRONTAB} /usr/bin/flock -n /var/run/backup.lock /bin/backup >> /var/log/cron.log 2>&1" > /var/spool/cron/crontabs/root
 
 # Make sure the file exists before we start tail
